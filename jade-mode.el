c0b9cfc7552eb605bcfc6679ffb26e7a87e47d83
Shander Lam <shanderlam@gmail.com>
2012-06-04 20:36:09 +0800

    Modify mather for doctype.

--- jade-mode.el
+++ jade-mode.el
@@ -26,7 +26,7 @@
   (string-match-p "^[ ]*$" (jade-line-as-string)))
 
 (setq jade-font-lock-keywords
-      `((,"!!!\\( \\(default\\|5\\|transitional\\)\\)?" 0 font-lock-comment-face) ;; doctype
+      `((,"!!!\\( ?[A-Za-z0-9\-\_]*\\)?" 0 font-lock-comment-face) ;; doctype
         (,"#\\(\\w\\|_\\|-\\)*" . font-lock-variable-name-face) ;; id
         (,"\\(?:^[ {2,}]+\\(?:[a-z0-9_:\\-]*\\)\\)?\\(#[A-Za-z0-9\-\_]*[^ ]\\)" 1 font-lock-variable-name-face) ;; id
         (,"\\(?:^[ {2,}]+\\(?:[a-z0-9_:\\-]*\\)\\)?\\(\\.[A-Za-z0-9\-\_]*\\)" 1 font-lock-type-face) ;; class name
